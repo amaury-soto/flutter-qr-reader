97f7a180873f3deb77ef54087be3c49aea3f30b5
Amaury Soto Garay <amaury.soto@globant.com>
2022-12-29 18:02:32 -0500

    IMPORTAN: add Scan Model

--- lib/widgets/scan_button.dart
+++ lib/widgets/scan_button.dart
@@ -7,11 +7,12 @@ class ScanButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return FloatingActionButton(onPressed: ()async{
-      String barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
+      /* String barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
                                                     '#3D8BEF', 
                                                     'Cancelar', 
                                                     false, 
-                                                    ScanMode.QR);
+                                                    ScanMode.QR); */
+    final barcodeScanRes = 'https://fernando-herrera.com';                                         
     print('barcodeScanRes $barcodeScanRes');
     }, child: Icon(Icons.filter_center_focus),);
   }
